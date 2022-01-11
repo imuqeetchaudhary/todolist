7622c4cab0db77e97a856fe8478d417cfbb82bdc
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-11 16:07:19 +0500

    implementaion of adding new todo item

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = () => {
+  const todosArray = [
+    {
+      id: 1,
+      title: "ToDo 1",
+    },
+    {
+      id: 2,
+      title: "ToDo 2",
+    },
+    {
+      id: 3,
+      title: "ToDo 3",
+    },
+  ];
+
+  const [todos, setTodos] = useState(todosArray);
+  const [newTodo, setNewTodo] = useState({ id: null, title: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo({ id: null, title: "" });
+  };
+
+  const handleChange = (e) => {
+    setNewTodo({ id: todos.length + 1, title: e.target.value });
+  };
+
   return (
     <div>
       <div className="box" id="heading">
@@ -8,25 +36,20 @@ const List = () => {
       </div>
 
       <div className="box">
-        <div className="item">
-          <input type="checkbox" />
-          <p>Item 1</p>
-        </div>
-        <div className="item">
-          <input type="checkbox" />
-          <p>Item 2</p>
-        </div>
-        <div className="item">
-          <input type="checkbox" />
-          <p>Item 3</p>
-        </div>
+        {todos.map((todo) => (
+          <div className="item" key={todo.id}>
+            <input type="checkbox" />
+            <p>{todo.title}</p>
+          </div>
+        ))}
 
-        <form className="item">
+        <form className="item" onSubmit={handleSubmit}>
           <input
             type="text"
-            name="newItem"
-            placeholder="New Item"
+            value={newTodo.title}
+            placeholder="New Todo Item"
             autoComplete="off"
+            onChange={handleChange}
           />
           <button type="submit">+</button>
         </form>
